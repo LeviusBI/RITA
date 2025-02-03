f7bdc7593149bfae56923e28d0659d69c230023f
Lev <levlewleff@gmail.com>
2025-02-03 14:31:17 +0300

    Addef fastp and STAR strings

--- RITA_align.sh
+++ RITA_align.sh
@@ -1,36 +1,32 @@
 #!/bin/bash
 
+#!/bin/bash
+
+# Инициализация переменных
 srrNumber=""
+threads=""
 findFlag=false
 existingFlag=false
 notDownloadFlag=false
+noGenDir=""
 r1=""
 r2=""
-noGgen=false
 
-RITA=~/RITA
+# Функция для вывода справки
 usage() {
-    echo "usage: $0 --srrNumber <SRA number> [--find] [--existing] [--notDownload] [-r1 <*.fastq.gz>] [-r2 <fastq.gz>]"
-    echo "  --srrNumber <number>   (obligatory) SRA id to download via fasterq-dump, create folders for fastp reports and STAR alignments"
-    echo "  --find                Starts RITA_find.sh by the end of RITA_align.sh script"
-    echo "  --existing            Use this flag to specify that you want to find only existing isoforms"
-    echo "  --notDownload         Use this flag if you have already downloaded your data and you want to skip downloading stage"
-    echo "  -r1 <*.fastq.gz>            Only with --nowDownload flag: "
-    echo "  -r2 <*.fastq.gz>            Указывает второй файл."
-    echo "  --noGgen              Use this if you do not want to generate new Genome
-     (only in cases if you know that length of reads is equal in all your fastq files)"
+    echo "Использование: $0 --srrNumber <номер> --threads <количество> [--find] [--existing] [--notDownload -r1 <файл1> -r2 <файл2>] [--noGen <директория>]"
     exit 1
 }
 
-if [[ $# -eq 0 ]]; then
-    usage
-fi
-
-
+# Обработка аргументов
 while [[ $# -gt 0 ]]; do
     case "$1" in
         --srrNumber)
             srrNumber="$2"
+            shift 2
+            ;;
+        --threads)
+            threads="$2"
             shift 2
             ;;
         --find)
@@ -44,13 +40,13 @@ while [[ $# -gt 0 ]]; do
         --notDownload)
             notDownloadFlag=true
             shift
+            r1="$1"
+            shift
+            r2="$1"
+            shift
             ;;
-        -r1)
-            r1="$2"
-            shift 2
-            ;;
-        -r2)
-            r2="$2"
+        --noGen)
+            noGenDir="$2"
             shift 2
             ;;
         *)
@@ -60,11 +56,35 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-# Проверка обязательного флага
-if [[ -z "$srrNumber" ]]; then
-    echo "Ошибка: флаг --srrNumber обязателен."
+# Проверка обязательных флагов
+if [[ -z "$srrNumber" || -z "$threads" ]]; then
+    echo "Ошибка: обязательные флаги --srrNumber и --threads должны быть указаны."
     usage
 fi
+
+# Проверка флага --noGen
+if [[ -n "$noGenDir" && ! -d "$noGenDir" ]]; then
+    echo "Ошибка: указанный путь для --noGen не является директорией."
+    exit 1
+fi
+
+# Проверка флага --notDownload
+if $notDownloadFlag; then
+    if [[ -z "$r1" || -z "$r2" ]]; then
+        echo "Ошибка: при использовании --notDownload необходимо указать -r1 и -r2."
+        exit 1
+    fi
+fi
+
+# Вывод значений флагов для проверки
+echo "srrNumber: $srrNumber"
+echo "threads: $threads"
+echo "findFlag: $findFlag"
+echo "existingFlag: $existingFlag"
+echo "notDownloadFlag: $notDownloadFlag"
+echo "noGenDir: $noGenDir"
+echo "r1: $r1"
+echo "r2: $r2"
 
 
 
@@ -74,13 +94,40 @@ STAR=$RITA/tools/STAR-2.7.11b/bin/Linux_x86_64/STAR
 STRINGTIE=$RITA/tools/
 
 mkdir $RITA/reads/$srrNumber
-$FASTERQ --split-files $srrNumber -O $RITA/reads/$srrNumber
+mkdir $RITA/fastp_reports/$srrNumber
+
+$FASTERQ -p --split-files $srrNumber -O $RITA/reads/$srrNumber
 
 cd $RITA/reads/$srrNumber
 
+gzip $RITA/reads/$srrNumber/*
+
+CURRENT=$RITA/reads/$srrNumber
+CUR_REPS=$RITA/fastp_reports/$srrNumber
+
+file_count=$(find "$CURRENT" -maxdepth 1 -type f | wc -l)
+
+case $file_count in
+    1) 
+        echo "Found 1 file, all downstream analysis will be done for one file with reads"
+        $FASTP -w 16 -i "$CURRENT"/*.gz -o "$srrNumber"_TRIMMED.fastq.gz -j "$CUR_REPS"/"$srrNumber"_FASTPREP.json -h "$CUR_REPS"/"$srrNumber"_FASTPREP.html
+        rm -rf !("$CURRENT"/"$srrNumber"_TRIMMED.fastq.gz)
+        ;;
+    2)
+        echo "Found 2 files, all downstrean analysis will be done for two files with reads"
+        file_list=($(find "$CURRENT" -maxdepth 1 -type f))
+        $FASTP -w 16 -i ${file_list[0]} -I ${file_list[1]} -o "$srrNumber"_1_TRIMMED.fastq.gz -O "$srrNumber"_2_TRIMMED.fastq.gz -j "$CUR_REPS"/"$srrNumber"_FASTPREP.json -h "$CUR_REPS"/"$srrNumber"_FASTPREP.html
+        rm -rf !("$CURRENT"/"$srrNumber"_?_TRIMMED.fastq.gz)
+        ;;
+    *)
+        echo "Error: found more than 2 files, check ${CURRENT}, delete all and restart. Do fastp manually if more than 2 files"
+        exit 1
+        ;;
+esac
+
 $FASTP -i 
 
-json_rep=$RITA/fastp_reports/$srrNumber/FASTP_${srrNumber}_report.json
+json_rep=$RITA/fastp_reports/$srrNumber/${srrNumber}_FASTPREP.json
 
 numbers=($( grep 'read1_mean_length' $json_rep | grep -o '[0-9]\{2,3\}' ))
 
@@ -92,23 +139,21 @@ min=$(( number1 < number2 ? number1 : number2 ))
 
 READLEN=$((min_number - 1))
 
-# # FASTP 0.23.4
-# FASTP=~/fastp
+mkdir "$RITA"/STAR_results/"$srrNumber"
 
-# # STAR 2.7.11b
-# STAR=~/STAR/bin/Linux_x86_64/STAR
+GENDIR="$RITA"/STAR_results/"$srrNumber"
+
+ulimit -n 65535
+
+"$STAR"  --runMode genomeGenerate   --runThreadN "$threads"   --genomeDir "$GENDIR"   --genomeFastaFiles referneces/Homo_sapiens.GRCh38.dna.primary_assembly.fa      --sjdbGTFfile referneces/Homo_sapiens.GRCh38.112.gtf   --sjdbOverhang 99
+
+$STAR --genomeDir ./genomeDir/ --readFilesCommand zcat --readFilesIn ./FASTQ_files/TRIMMED_SRR25_1.fastq.gz ./FASTQ_files/TRIMMED_SRR25_2.fastq.gz --outSAMtype BAM SortedByCoordinate --limitBAMsortRAM 128000000000 --outSAMunmapped Within --twopassMode Basic --quantMode TranscriptomeSAM --outSAMstrandField intronMotif --runThreadN 100
 
 
 
-# GENOMEFA=Homo_sapiens.GRCh38.dna.toplevel.fa
-# GENOMEGTF=Homo_sapiens.GRCh38.111.chr_patch_hapl_scaff.gtf
-# GENOMEFAURL=https://ftp.ensembl.org/pub/release-111/fasta/homo_sapiens/dna/${GENOMEFA}.gz
-# GENOMEGTFURL=https://ftp.ensembl.org/pub/release-111/gtf/homo_sapiens/${GENOMEGTF}.gz
 
-# TRANSCRIPTS=gencode.v45.transcripts.fa
-# TRANSCRIPTSURL=https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_45/${TRANSCRIPTS}.gz
-# GENOMEFA=GRCh38.primary_assembly.genome.fa
-# GENOMEFAURL=https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_45/${GENOMEFA}.gz
+
+
 
 # # Set length of reads, in case of variable length set the maximum length
 # READLEN=151 
